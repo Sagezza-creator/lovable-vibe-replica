3f56cf007ee7d332ef61c70087ba360c29f5778e
Sagezza-creator <an.sagezza@gmail.com>
2025-04-30 10:21:16 +0300

    Правка текста

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -74,7 +74,7 @@ const AboutSection = () => {
             </div>
             <div className="mt-8 flex flex-wrap gap-4">
               <div className="px-4 py-2 bg-brand-50 text-brand-700 rounded-full text-sm font-medium">
-                Эпигенетика
+                Психология
               </div>
               <div className="px-4 py-2 bg-brand-50 text-brand-700 rounded-full text-sm font-medium">
                 Работа с подсознанием
