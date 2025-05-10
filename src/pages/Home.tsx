9a3b1f1d6915d7eaafbab2f6ec92de7ec1e8235a
Sagezza-creator <an.sagezza@gmail.com>
2025-05-10 06:47:05 +0300

    бэкап

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,8 +8,8 @@ import ComparisonSection from '@/components/ComparisonSection';
 import ReviewsSection from '@/components/ReviewsSection';
 import FAQSection from '@/components/FAQSection';
 import CallToAction from '@/components/CallToAction';
-import CorrectionSection from '@/components/CorrectionSection';
 import MatricesSection from '@/components/MatricesSection';
+import CorrectionSection from '@/components/CorrectionSection';
 
 const Home = () => {
   const parallaxRef = useRef<HTMLDivElement>(null);
